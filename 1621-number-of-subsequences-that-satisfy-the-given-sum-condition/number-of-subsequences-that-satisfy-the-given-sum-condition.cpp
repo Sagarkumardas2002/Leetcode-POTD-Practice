2c0a527e2c341c059a7874572cbb11e41c9682df
SAGAR KUMAR DAS <108587278+Sagarkumardas2002@users.noreply.github.com>
2025-06-29 23:29:08 +0530

    Time: 31 ms (62.49%) | Memory: 54.3 MB (34.63%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,27 +1,44 @@
 class Solution {
 public:
     int numSubseq(vector<int>& nums, int target) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        vector<int> powof2(n);
-        int MOD = 1e9 + 7;
+        const int MOD = 1e9 + 7;  // To avoid overflow, we take modulo of final answers
 
-        powof2[0] = 1;
-        for (int i = 1; i < n; i++) {
-            powof2[i] = (powof2[i - 1] * 2) % MOD;
+        // Step 1: Sort the input array so we can use two pointers logic
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+
+        // Step 2: Precompute powers of 2 modulo MOD
+        // pow2[i] = 2^i % MOD
+        vector<int> pow2(n);
+        pow2[0] = 1;
+        for (int i = 1; i < n; ++i) {
+            pow2[i] = (pow2[i - 1] * 2) % MOD;
         }
 
-        int right = n - 1;
+        // Step 3: Use two pointers approach to count valid subsequences
         int left = 0;
-        int count = 0;
+        int right = n - 1;
+        int totalValidSubsequences = 0;
+
         while (left <= right) {
-            if (nums[left] + nums[right] <= target) {
-                count = (count + powof2[right - left]) % MOD;
-                left++;
+            int minElement = nums[left];
+            int maxElement = nums[right];
+
+            // Check if the sum of min and max elements is within the target
+            if (minElement + maxElement <= target) {
+                // All subsequences formed using elements between left and right (inclusive)
+                // where min is nums[left] and max is nums[right] will be valid
+                // Total = 2^(right - left) possible subsequences
+                int numOfSubseq = pow2[right - left];
+                totalValidSubsequences = (totalValidSubsequences + numOfSubseq) % MOD;
+
+                ++left;
             } else {
-                right--;
+                --right;
             }
         }
-        return count;
+
+        return totalValidSubsequences;
     }
 };
