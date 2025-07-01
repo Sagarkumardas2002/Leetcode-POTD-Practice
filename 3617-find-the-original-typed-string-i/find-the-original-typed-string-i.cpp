b2bbdb28f11c3e452f96e947d8311333337b63ae
SAGAR KUMAR DAS <108587278+Sagarkumardas2002@users.noreply.github.com>
2025-07-01 18:45:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (29.69%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     int possibleStringCount(string word) {
-        int n=word.size();
-        int count=1;
-        for(int i=0;i<n;){
-            int j=i;
-            while(j<n && word[j]==word[i]) j++;
-            int length=j-i;
+        int n = word.size();
 
-            if(length>1){
-                count+=(length-1);
+        // start with 1: the original word is always one possibility
+        int count = 1;  
+        
+        // Try reducing one or more repeats in a single group (only one group allowed)
+        for (int i = 0; i < n;) {
+            int j = i;
+
+            // Count the length of a repeating character group
+            while (j < n && word[j] == word[i]) j++;
+            int len = j - i;
+            if (len > 1) {
+                count += (len - 1);  // We can reduce this group in (len - 1) ways
             }
-            i=j;
+            
+            i = j;  // Move to the next group
         }
+
         return count;
     }
 };
