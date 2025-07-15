0302a5d7bf9c206f3451897b261804c3948cfae5
SAGAR KUMAR DAS <108587278+Sagarkumardas2002@users.noreply.github.com>
2025-07-15 15:43:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (5.83%) - LeetSync

--- 3396-valid-word/valid-word.cpp
+++ 3396-valid-word/valid-word.cpp
@@ -1,35 +1,21 @@
 class Solution {
 public:
     bool isValid(string word) {
-        set<char> vowel = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
-        set<char> num = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-        set<char> consonent = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
-                               'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w',
-                               'x', 'y', 'z', 'B', 'C', 'D', 'F', 'G', 'H',
-                               'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S',
-                               'T', 'V', 'W', 'X', 'Y', 'Z'};
+        unordered_set<char> vowels = {'a','e','i','o','u','A','E','I','O','U'};
+        unordered_set<char> digits = {'0','1','2','3','4','5','6','7','8','9'};
 
-        int vCount = 0;
-        int numCount = 0;
-        int consCount = 0;
-        int otherCount=0;
+        int vowelCount = 0, consonantCount = 0;
 
-        for (auto& it : word) {
-            if (vowel.find(it) != vowel.end()) {
-                vCount++;
-            } else if (num.find(it) != num.end()) {
-                numCount++;
-            } else if (consonent.find(it) != consonent.end()) {
-                consCount++;
-            }
-            else{
-                otherCount++;
+        for (char ch : word) {
+            if (!isalpha(ch) && !isdigit(ch)) return false;
+
+            if (vowels.count(ch)) {
+                vowelCount++;
+            } else if (isalpha(ch)) {
+                consonantCount++;
             }
         }
 
-        if (word.size() >= 3 && vCount >= 1 && consCount >= 1 && otherCount==0) {
-            return true;
-        }
-        return false;
+        return word.size() >= 3 && vowelCount >= 1 && consonantCount >= 1;
     }
 };
