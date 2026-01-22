ce070a05eb48a4aeb4c2146340096479a80fb472
SAGAR KUMAR DAS <108587278+Sagarkumardas2002@users.noreply.github.com>
2026-01-22 13:22:04 +0530

    Time: 0 ms (100.00%) | Memory: 26.6 MB (41.46%) - LeetSync

---
+++ 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isNonDecreasing(vector<int>& nums) {
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    int minimumPairRemoval(vector<int>& nums) {
+        int operations = 0;
+        while (!isNonDecreasing(nums) && nums.size() > 1) {
+            int mini = INT_MAX;
+            int idx = -1;
+            for (int i = 0; i < nums.size() - 1; i++) {
+                int sum = nums[i] + nums[i + 1];
+                if (sum < mini) {
+                    mini = sum;
+                    idx = i;
+                }
+            }
+            nums[idx] = nums[idx] + nums[idx + 1];
+            nums.erase(nums.begin() + idx + 1);
+            operations++;
+        }
+        return operations;
+    }
+};
